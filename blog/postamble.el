13c40ac62ea80a121c00993ee9977790dc476542
gp <giulio.pietroiusti@gmail.com>
2022-11-13 20:30:18 +0100

    sicp 1.2.1.

---
+++ blog/postamble.el
@@ -0,0 +1,1 @@
+(setq org-html-postamble-format '(("en" "<p>Please, send send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> if you have any comment.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
