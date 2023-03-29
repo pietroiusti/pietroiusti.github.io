e8d7080abd671ae1de8f3d77a21cd564ec8f9b46
gp <giulio.pietroiusti@gmail.com>
2023-03-29 23:32:05 +0200

    Post 13.

--- blog/postamble.el
+++ blog/postamble.el
@@ -1,1 +1,4 @@
 (setq org-html-postamble-format '(("en" "<p>Please, send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> if you have any comment.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
+
+
+(setq org-html-postamble-format '(("en" "<p>Send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> for comments.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
