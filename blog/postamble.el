451fd7e1fb5bd79d26ecadff56fe3d87f50a2774
gp <giulio.pietroiusti@gmail.com>
2024-06-16 23:23:39 +0200

    adjust backlinks

--- blog/postamble.el
+++ blog/postamble.el
@@ -2,3 +2,10 @@
 
 
 (setq org-html-postamble-format '(("en" "<p>Send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> for comments.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
+
+
+(setq org-html-postamble-format '(("en" "<div style=\"text-align: center;\"><a href=\"./posts.html\">←</a></div><p>Send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> for comments.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
+
+(setq org-html-preamble-format '(("en" "<div style=\"text-align: center;\"><a href=\"./posts.html\">←</a></div>")))
+
+(setq org-html-preamble-format '(("en" "")))
