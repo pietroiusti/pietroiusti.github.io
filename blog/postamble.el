0f9adf419e249dde497077559bf70264a7712176
gp <giulio.pietroiusti@gmail.com>
2024-09-18 20:49:23 +0200

    post 36

--- blog/postamble.el
+++ blog/postamble.el
@@ -1,11 +1,11 @@
-(setq org-html-postamble-format '(("en" "<p>Please, send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> if you have any comment.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
+;; (setq org-html-postamble-format '(("en" "<p>Please, send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> if you have any comment.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
 
 
-(setq org-html-postamble-format '(("en" "<p>Send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> for comments.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
+;; (setq org-html-postamble-format '(("en" "<p>Send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> for comments.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
 
 
 (setq org-html-postamble-format '(("en" "<div style=\"text-align: center;\"><a href=\"./posts.html\">←</a></div><p>Send me an <a href=\"mailto:giulio.pietroiusti@gmail.com\">email</a> for comments.</p> <p>Created with <span class=\"creator\">%c</span></p>")))
 
-(setq org-html-preamble-format '(("en" "<div style=\"text-align: center;\"><a href=\"./posts.html\">←</a></div>")))
+;; (setq org-html-preamble-format '(("en" "<div style=\"text-align: center;\"><a href=\"./posts.html\">←</a></div>")))
 
-(setq org-html-preamble-format '(("en" "")))
+;; (setq org-html-preamble-format '(("en" "")))
